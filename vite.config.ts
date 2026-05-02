e6ccf097f5e3beece056bb9b1bccc4ad05c12a6f
zryshuaige <151923106+zryshuaige@users.noreply.github.com>
2026-05-03 00:01:12 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+ export 默认 defineConfig({
+   base: '/zrymax/',
+   plugins: [vue()],
+ })
